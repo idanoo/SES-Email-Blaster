afe005d78521d0c8b1c634620f10b47099e6d7eb
idanoo <daniel@m2.nz>
2022-11-14 13:11:42 +1300

    Abstract tooling

---
+++ README.md
@@ -0,0 +1,8 @@
+# SES-email-blast
+
+Used to send bulk test emails for.. purposes
+    
+Usage:
+```
+$ bash send.sh <AWS Profile> <Num of msgs> <from email> <to email>
+```
--- send.sh
+++ send.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-PROFILE=$1
-FROM=$2
+PROFILE=$1 # which AWS profile
+NUMBER=$2 # how many to send
+FROM=$3 # who we sending from
+TO=$4 # who we sending to
 
-for i in {1..100}
+x=1
+sed -i "s/REPLACE_WITH_SENDER/$TO/g" destination.json
+echo "Sending $NUMBER messages to $TO from $FROM"
+for i in $(seq 1 $NUMBER)
 do 
+    sed -i "s/$x /$i /g" message.json
+    x=$i
     aws ses --region us-west-2 --profile $PROFILE send-email --from $FROM --destination file://destination.json --message file://message.json
 done
+
+sed -i "s/$TO/REPLACE_WITH_SENDER/g" destination.json
+sed -i "s/$x /1 /g" message.json
